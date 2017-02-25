a1132e197f03312ad1d15ff47ab23be0d2e8b94c
Harish Palani <harishpalani3@gmail.com>
2017-02-25 21:51:59 +0000

    Oops, forgot to push updated Stack files

--- Stack.cpp
+++ Stack.cpp
@@ -2,13 +2,24 @@
 
 char Stack::peek() {
     if (head == NULL) { return '\0'; } 
-    return head->token;
+    return head->c;
 }
 
 char Stack::pop() {
-    
+    Node* n = new Node();
+    n->next = head;
+    n->c = token;
+    head = n;
 }
 
 void Stack::push(char c) {
+    if (head != NULL){
+        char temp = head->c;
+        Node* topOfStack = head;
+        head = head->next;
+        delete topOfStack;
+        return temp;
+    }
     
+    return 0;
 }
--- Stack.h
+++ Stack.h
@@ -6,7 +6,7 @@
 struct Node {
     char c;
     Node* next = NULL;
-}
+};
 
 class Stack {
     public:
