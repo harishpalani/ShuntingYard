5876222b24531e68d4ccb5d1776de4c794da609b
Harish Palani <harishpalani3@gmail.com>
2017-02-25 21:11:40 +0000

    Program infrastructure implemented

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
+/**
+ * Shunting Yard Algorithm
+ * Translating mathematical expressions from infix to postfix notation, 
+ * considering basic arithmetic expressions, integers, and parantheses.
+ * 
+ * @author Harish Palani
+ * @version February 2017
+ */
 #include <iostream>
+#include <cstring>
+#include "Stack.h"
 
 using namespace std;
 
+// Function prototypes
+char* toPostfix(char *infix);
+
 int main() {
-    
+    char infix[128];
+    while (true) {
+        // Get infix expression
+        cout << "Enter an infix expression: ";
+        cin.getline(infix, 128);
+        cout << "Infix: " << infix << endl;
+        
+        // Convert infix to postfix & display
+        char* postfix = toPosfix(input);
+        cout << "Postfix: " << postfix << endl;
+        delete[] postfix;
+    }
     return 0;
+}
+
+char* toPostfix(char *infix) {
+    
 }
